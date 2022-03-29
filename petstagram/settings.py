55f647c25fe12016fc686b271a52f505484045f8
mirooka <95923588+mirooka@users.noreply.github.com>
2022-03-29 12:35:49 +0300

    added app to allowed hosts

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-rj-qxwx608o#=_@53!6ees31cjm044k-z+74&t%@127%uc^a*&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "petstagram-miro.herokuapp.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
